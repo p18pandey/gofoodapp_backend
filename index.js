6b05659fb10a294ce9f066b8df3094ae184b49ce
Prakash <prakashpandey092001@gmail.com>
2023-07-09 14:53:46 +0530

    final update cors

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ mongoDB();
 // app.use(cors());
 app.use((req,res,next)=>{
   // res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Origin","https://gofoodp42.netlify.app");
+  // res.setHeader("Access-Control-Allow-Origin","https://gofoodp42.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin","https://gofood18p.netlify.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
