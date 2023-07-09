c5d46968133b9d38e858cafdce55169da633b6bb
Prakash <prakashpandey092001@gmail.com>
2023-07-09 14:50:55 +0530

    cors update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mongoDB();
   
 // }
 // ));
-app.use(cors());
+// app.use(cors());
 app.use((req,res,next)=>{
   // res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
   res.setHeader("Access-Control-Allow-Origin","https://gofoodp42.netlify.app");
