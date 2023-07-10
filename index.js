c9a5c32a62c294c93459279500e912e96330b0f6
Prakash <prakashpandey092001@gmail.com>
2023-07-10 14:47:41 +0530

    final change

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoDB();
 app.use((req,res,next)=>{
   // res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
   // res.setHeader("Access-Control-Allow-Origin","https://gofoodp42.netlify.app");
-  res.setHeader("Access-Control-Allow-Origin","https://gofood18p.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin","https://gofood82p.netlify.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
